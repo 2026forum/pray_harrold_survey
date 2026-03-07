3af7efa90640c1cb97462a0a41e7f1cf4a7dd398
Sam Zaccone <samrzaccone@gmail.com>
2026-03-06 23:57:39 -0500

    implement full comments

--- lib/ui/subject_detail_screen.dart
+++ lib/ui/subject_detail_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pray_harrold_survey/constants.dart';
 import 'package:pray_harrold_survey/features/auth/auth_controller.dart';
+import 'package:pray_harrold_survey/features/comments/comments_controller.dart';
 import 'package:pray_harrold_survey/features/comments/comments_repository.dart';
 import 'package:pray_harrold_survey/features/subjects/subjects_controller.dart';
 import 'package:pray_harrold_survey/ui/tiles/comment_tile.dart';
 import 'package:pray_harrold_survey/util/error_loader.dart';
+import 'package:pray_harrold_survey/util/show_messages.dart';
+import 'package:pray_harrold_survey/util/text_validation.dart';
 
+import '../models/comment.dart';
 import '../models/subject.dart';
 
 const kComment = "comment";
@@ -22,6 +27,57 @@ class SubjectDetailScreen extends ConsumerStatefulWidget {
 }
 
 class _SubjectDetailScreenState extends ConsumerState<SubjectDetailScreen> {
+  final _commentController = TextEditingController();
+
+
+  _makeComment() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text("what do you say?"),
+                TextField(controller: _commentController, maxLines: 3, maxLength: kCommentMaxChar),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Nevermind'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (isValidTextValue(_commentController)) {
+                          ref.read(commentsControllerProvider(widget.subject.subjectId)).addComment(validTextValueReturner(_commentController));
+                          Navigator.of(context).pop();
+                        }
+                      },
+                      child: const Text('OK'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _authoredTile(Comment comment) {
+    return ListTile(
+      title: Center(child: Text(comment.commentText)),
+      subtitle: Center(child: Text(comment.author)),
+      onTap: () => _makeComment(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final person = ref.watch(personProvider)!;
@@ -33,7 +89,9 @@ class _SubjectDetailScreenState extends ConsumerState<SubjectDetailScreen> {
             padding: const EdgeInsets.all(8.0),
             child: PopupMenuButton<String>(
               onSelected: (value) {
-                if (value == "comment") {}
+                if (value == "comment") {
+                  _makeComment();
+                }
                 if (value == "undoVote") {
                   if (widget.subject.agreement.contains(person.uid)) {
                     //TODO make popup
@@ -42,6 +100,7 @@ class _SubjectDetailScreenState extends ConsumerState<SubjectDetailScreen> {
                   if (widget.subject.disagreement.contains(person.uid)) {
                     ref.read(subjectsControllerProvider.notifier).unDisagree(widget.subject.subjectId);
                   }
+                  showSnackyBar(context, "Judgment Undone");
                 }
               },
               itemBuilder: (context) => [
@@ -68,12 +127,10 @@ class _SubjectDetailScreenState extends ConsumerState<SubjectDetailScreen> {
                         itemCount: commentList.length,
                         itemBuilder: (context, index) {
                           final comment = commentList[index];
-                          return Column(
-                            children: [
-                              CommentTile(comment, widget.subject),
-                              const Divider(),
-                            ],
-                          );
+                          if (comment.authorId == person.uid) {
+                            return Column(children: [_authoredTile(comment), Divider()]);
+                          }
+                          return Column(children: [CommentTile(comment, widget.subject), const Divider()]);
                         },
                       );
                     },
@@ -85,5 +142,11 @@ class _SubjectDetailScreenState extends ConsumerState<SubjectDetailScreen> {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _commentController.dispose();
   }
 }
